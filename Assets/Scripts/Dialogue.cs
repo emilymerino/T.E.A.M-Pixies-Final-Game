cab3e4520cba3a2565f6b464605cb7a61ed4baa1
Michelle-Phung <MichellePhung@cmail.carleton.ca>
2026-02-26 13:12:07 -0500

    Created main menu and dialogue template

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public string name;
+    public string[] sentences;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
