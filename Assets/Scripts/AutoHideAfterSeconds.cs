2257bed0c1bbe5e1c42f57ae43e1c8e47f5963f6
TashfaAhmad <tahmadsmn@gmail.com>
2026-03-01 17:54:11 -0500

    Intro Sequence Setup

---
+++ Assets/Scripts/AutoHideAfterSeconds.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoHideAfterSeconds : MonoBehaviour
+{
+    [SerializeField] private float seconds = 3f;
+    [SerializeField] private GameObject nextObject;
+
+    private void OnEnable()
+    {
+        StartCoroutine(Hide());
+    }
+
+    private IEnumerator Hide()
+    {
+        yield return new WaitForSeconds(seconds);
+
+        gameObject.SetActive(false);
+
+        if (nextObject != null) 
+            nextObject.SetActive(true);
+    }
+}
