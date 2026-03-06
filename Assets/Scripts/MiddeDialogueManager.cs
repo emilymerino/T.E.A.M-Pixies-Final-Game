a0ad0ad9dcb2db858c685ecd1bb39321b2792cbe
areenascorner <areenakhokhar@cmail.carleton.ca>
2026-03-05 21:01:53 -0500

    Added clue system to all scenes
    clue system

--- Assets/Scripts/MiddeDialogueManager.cs
+++ Assets/Scripts/MiddeDialogueManager.cs
@@ -26,6 +26,17 @@ public class MiddeDialogueManager : MonoBehaviour
     [SerializeField] GameObject textBox;
     [SerializeField] private SMDialogueAutoNext autoNext;
 
+    public ClueData Clue1;
+    public ClueData Clue2;
+    public ClueData Clue3;
+    public ClueData Clue4;
+    public ClueData Clue5;
+    public ClueData Clue6;
+    public ClueData Clue7;
+    public ClueData Clue8;
+    public ClueData Clue9;
+    public ClueData Clue10;
+    public ClueData Clue11;
 
 
     void Start()
@@ -55,6 +66,13 @@ public class MiddeDialogueManager : MonoBehaviour
         mainText.SetActive(true);
 
         yield return StartCoroutine(currentDialogue("", 6, "You pocket the note and rush back to the meeting room."));
+
+        yield return new WaitForSeconds(2);
+
+        // unlock clue
+        ClueManager.Instance.UnlockClue(Clue2);
+
+
         classroom.SetActive(true);
         bathroom.SetActive(false);
         yield return StartCoroutine(currentDialogue("", 7, "You pull open the door. You see that the student council meeting has started."));
@@ -68,6 +86,14 @@ public class MiddeDialogueManager : MonoBehaviour
         lou.SetActive(false);
         missEvelyn.SetActive(true);
         yield return StartCoroutine(currentDialogue("Miss Evelyn", 6, "Let's not jump to dramatic conclusions."));
+
+
+        yield return new WaitForSeconds(2);
+
+        // unlock clue
+        ClueManager.Instance.UnlockClue(Clue4);
+
+
         missEvelyn.SetActive(false);
         archie.SetActive(true);
         yield return StartCoroutine(currentDialogue("Archie", 7, "Hm... I heard a noise down the hall earlier around 10 minutes ago."));
@@ -77,6 +103,14 @@ public class MiddeDialogueManager : MonoBehaviour
         quinton.SetActive(false);
         zeke.SetActive(true);
         yield return StartCoroutine(currentDialogue("Zeke", 7, "The vampire thing worries me. Hey Lou, aren't you allergic to garlic?"));
+
+
+        yield return new WaitForSeconds(2);
+
+        // unlock clue
+        ClueManager.Instance.UnlockClue(Clue6); 
+
+
         zeke.SetActive(false);
         yield return StartCoroutine(currentDialogue("", 8, "The room erupted into choas. Everyone was acting weird, you can't seem to trust anyone."));
 
