a01ea9f6d5c9cf8e341cd2c3a816c650ea10e4a3
emilymerino <emilymerino@cmail.carleton.ca>
2026-03-12 10:04:01 -0400

    added ready button to locker code and removed timer

--- Assets/Scripts/LCClueUnlockedPopUp.cs
+++ Assets/Scripts/LCClueUnlockedPopUp.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class LCPlayerWonPopUp : MonoBehaviour
+public class LCClueUnlockedPopUp : MonoBehaviour
 {
     public float timeRemaining = 2f;
     public TextMeshProUGUI PlayerWon;
