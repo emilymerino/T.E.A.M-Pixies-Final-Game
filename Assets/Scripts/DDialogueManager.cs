0025887606b5dd7e039298d97009c9d6eb0d3088
Michelle-Phung <MichellePhung@cmail.carleton.ca>
2026-03-12 11:37:36 -0400

    Updated text boxes and next button for beginning and middle scenes

--- Assets/Scripts/DDialogueManager.cs
+++ Assets/Scripts/DDialogueManager.cs
@@ -49,13 +49,17 @@ public class DDialogueManager : MonoBehaviour
         //yield return StartCoroutine(currentDialogue("Mei", 5, "Oh it’s nothing, Eloise! Anyways I’ll be back, just going to go to the washroom."));
         //yield return StartCoroutine(currentDialogue("", 5, "Mei scurries deeper into the hallway in the direction of the bathroom. You head into the meeting room and wait."));
         //yield return StartCoroutine(currentDialogue("", 5, "Before you know it, you suddenly hear a blood-curdling scream. It came from the bathroom."));
-        yield return StartCoroutine(currentDialogue("", 8, "Rain pours outside as you wait for the student council meeting to start. No one else was in the room just yet."));
-        yield return StartCoroutine(currentDialogue("", 8, "Suddenly, a blood-curdling scream pierecd through the hallway. It sounded like it came from the bathroom."));
+        yield return StartCoroutine(currentDialogue("", "Rain pours outside as you wait for the student council meeting to start. No one else was in the room just yet."));
+        skip = false;
+        yield return StartCoroutine(currentDialogue("", "Suddenly, a blood-curdling scream pierecd through the hallway. It sounded like it came from the bathroom."));
+        skip = false;
 
         bathroom.SetActive(true);
         classroom.SetActive(false);
-        yield return StartCoroutine(currentDialogue("", 8, "You push open the bathroom doors. There you find your best friend Mei's lifeless body with a bite mark on her neck."));
-        yield return StartCoroutine(currentDialogue("", 8, "Panic sets in and you look around, some pieces of bloody, torn up paper catch your eye."));
+        yield return StartCoroutine(currentDialogue("", "You push open the bathroom doors. There you find your best friend Mei's lifeless body with a bite mark on her neck."));
+        skip = false;
+        yield return StartCoroutine(currentDialogue("", "Panic sets in and you look around, some pieces of bloody, torn up paper catch your eye."));
+        skip = false;
         //SceneManager.LoadSceneAsync("[Next Scene]");
 
         // unlock clue here
@@ -73,25 +77,13 @@ public class DDialogueManager : MonoBehaviour
         }
     }
 
-    IEnumerator currentDialogue(string name, int num, string dialogue)
+    IEnumerator currentDialogue(string name, string dialogue)
     {
-        int time = 0;
         mainText.GetComponent<TMP_Text>().text = dialogue;
         charName.GetComponent<TMP_Text>().text = name;
 
-        while(time < num)
-        {
-            if(skip == true)
-            {
-                skip = false;
-                Debug.Log("Skipped dialogue");
-                yield break;
-            }
-            yield return new WaitForSeconds(1);
-            time++;
-        }
-        Debug.Log("Finished dialogue");
-        yield break;
+        Debug.Log("Skipped dialogue");
+        yield return new WaitUntil(() => skip == true);
     }
 
 }
