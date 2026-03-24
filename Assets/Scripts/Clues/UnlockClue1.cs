526b52adfed7c41ac4a40ca897d7e4f7420ad769
areenascorner <areenakhokhar@cmail.carleton.ca>
2026-03-23 23:24:44 -0400

    Update UnlockClue1.cs

--- Assets/Scripts/Clues/UnlockClue1.cs
+++ Assets/Scripts/Clues/UnlockClue1.cs
@@ -8,6 +8,19 @@ public class UnlockClue1 : MonoBehaviour
     public Button myButton;
     public ClueData clue1;
     public ClueData clue2;
+    public ClueData clue3;
+    public ClueData clue4;
+    public ClueData clue5;
+    public ClueData clue6;
+    public ClueData clue7;
+    public ClueData clue8;
+    public ClueData clue9;
+    public ClueData clue10;
+    public ClueData clue11;
+    public ClueData clue12;
+    public ClueData clue13;
+    public ClueData clue14;
+   
 
     void Start()
     {
@@ -24,6 +37,18 @@ public class UnlockClue1 : MonoBehaviour
         {
             ClueManager.Instance.UnlockClue(clue1);
             ClueManager.Instance.UnlockClue(clue2);
+            ClueManager.Instance.UnlockClue(clue3);
+            ClueManager.Instance.UnlockClue(clue4);
+            ClueManager.Instance.UnlockClue(clue5);
+            ClueManager.Instance.UnlockClue(clue6);
+            ClueManager.Instance.UnlockClue(clue7);
+            ClueManager.Instance.UnlockClue(clue8);
+            ClueManager.Instance.UnlockClue(clue9);
+            ClueManager.Instance.UnlockClue(clue10);
+            ClueManager.Instance.UnlockClue(clue11);
+            ClueManager.Instance.UnlockClue(clue12);
+            ClueManager.Instance.UnlockClue(clue13);
+            ClueManager.Instance.UnlockClue(clue14);
 
         }
     }
