79dac806631d65d65673f6c226a3cebbea222b23
Michelle-Phung <MichellePhung@cmail.carleton.ca>
2026-03-24 21:58:16 -0400

    Added Second Conversation dialogue scenes

--- Assets/Scripts/Dialogue/FirstConversationsDialogue.cs
+++ Assets/Scripts/Dialogue/FirstConversationsDialogue.cs
@@ -231,7 +231,7 @@ public class FirstConversationsDialogue : MonoBehaviour
         yield return StartCoroutine(currentDialogue("Quinton", "But it was just to check some numbers on the council’s accounts. Something wasn’t adding up."));
         eloiseSuspicious.SetActive(true);
         quintonNeutral.SetActive(false);
-        yield return StartCoroutine(currentDialogue("Eloise", "Oh come on, Quinton"));
+        yield return StartCoroutine(currentDialogue("Eloise", "Oh come on, Quinton."));
         eloiseSuspicious.SetActive(false);
         quintonNeutral.SetActive(true);
         yield return StartCoroutine(currentDialogue("Quinton", "She started acting weird with me. I feel like she found something out that she shouldn’t have, and told me she wasn’t going to confront it herself. That’s all."));
