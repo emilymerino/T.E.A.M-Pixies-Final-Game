db9cc5c548f10c49d72109fdb9e0aeff3a4fcb04
emilymerino <emilymerino@cmail.carleton.ca>
2026-03-26 13:34:19 -0400

    adding question marks

--- Assets/Scripts/InteractionScenes/ISHoverGlow.cs
+++ Assets/Scripts/InteractionScenes/ISHoverGlow.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ISHoverGlow : MonoBehaviour
@@ -13,6 +11,9 @@ public class ISHoverGlow : MonoBehaviour
 
     void OnMouseEnter()
     {
+        // ❌ don't glow while talking
+        if (FirstConversationsDialogue.isTalking) return;
+
         Sprite.gameObject.SetActive(true);
         Debug.Log("Enter");
     }
