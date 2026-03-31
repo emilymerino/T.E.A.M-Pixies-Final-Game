a74d75d423448b24a7d3da8087b58677152ea809
areenascorner <areenakhokhar@cmail.carleton.ca>
2026-03-30 22:39:43 -0400

    Added game sound
    Added game sound scripts +

--- Assets/Scripts/Sound/SoundsManager.cs
+++ Assets/Scripts/Sound/SoundsManager.cs
@@ -7,7 +7,11 @@ public class SoundsManager : MonoBehaviour
 
     public static SoundsManager Instance;
 
-    public AudioSource musicSource;
+    public AudioSource musicSource; // music 
+
+    public AudioSource ambienceSource; // rain background sounds
+
+    public AudioSource sfxSource; // will store short sound effects
 
     void Awake()
     {
@@ -31,5 +35,27 @@ public class SoundsManager : MonoBehaviour
         musicSource.clip = clip;
         musicSource.loop = true;
         musicSource.Play();
+    }
+
+    public void PlayAmbience(AudioClip clip)
+    {
+        // not restartign
+
+        if (musicSource.clip == clip) return;
+
+        musicSource.clip = clip;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+
+    public void StopAmbience()
+    {
+        ambienceSource.Stop();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
     }
 }
